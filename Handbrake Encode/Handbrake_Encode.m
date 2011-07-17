c6d9f28f58d5027c9ffdf37ccf3b12f078da0294
Paul Grave <paul@stomer.net>
2011-07-17 22:48:07 +0100

    added: code comments

--- Handbrake Encode/Handbrake_Encode.m
+++ Handbrake Encode/Handbrake_Encode.m
@@ -11,18 +11,22 @@
 @implementation Handbrake_Encode
 @synthesize qualityTextField;
 
-
-
 - (IBAction)sliderMove:(id)sender {
 	if (![sender isKindOfClass:[NSSlider class]])
         return;
 	
-	double sliderValue = [(NSSlider *)sender doubleValue]; 
+	double sliderValue = [(NSSlider *)sender doubleValue];
+	
+	// we're interested in quarters
 	int tickInterval = 4;
+	
+	// round to the nearest 0.25
 	double roundedValue = round(sliderValue * tickInterval)/tickInterval;	
 	
+	// make sure the slider only hits values rounded to 0.25
 	[sender setDoubleValue:roundedValue];
 	
+	// we need to invert the scale since Handbrake's scale is 51 -> 0, whilst by default NSSlider is 0 -> 51
 	quality = fabs(roundedValue - 51);
 	NSString *qualityText = [NSString stringWithFormat:@"RF: %.2f", quality];
 
